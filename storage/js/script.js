69082e8cc5d8bdc5700fab7e879ad3ec908d1ebb
VnFood <tranloi.tphl@gmail.com>
2024-03-28 01:45:45 +0700

    Updated

--- storage/js/script.js
+++ storage/js/script.js
@@ -22,3 +22,68 @@ $(document).ready(function(){
     } // End if
   });
 });
+
+
+// === Ring animation [ START ] ===
+var animatedImage = document.getElementById('ring');
+var c = 1;
+var data
+
+function startAnimation() {
+  clearInterval(data);
+  data = setInterval(() => {
+    c++;
+    if (c > 95) {
+      c = 0;
+    }
+
+    if (c < 10) {
+      c = `0${c}`;
+    }
+
+
+    animatedImage.src = `./storage/imgs/ring/0${c}.webp`;
+    // console.log('loi ', c);
+  }, 100, null, true);
+}
+
+startAnimation();
+
+
+// === Ring animation [ END ] ===
+
+
+// === Star animation [ START ] ===
+//Get context and screen size;
+var ctx = cnv.getContext("2d");
+var W = window.innerWidth;
+var H = window.innerHeight;
+
+//Set Canvas and Background Color;
+cnv.width = W;
+cnv.height = 650;
+ctx.fillStyle = "#000";
+ctx.fillRect(0, 0, W, H);
+
+//Glow effect;
+ctx.shadowBlur = 10;
+ctx.shadowColor = "white";
+
+function animate() {
+  //Random position and size of stars;
+  let x = W * Math.random();
+  let y = H * Math.random();
+  // let r = 2.5 * Math.random();
+  let r = 0.55;
+
+  //Draw the stars;
+  ctx.beginPath();
+  ctx.fillStyle = "white";
+  ctx.arc(x, y, r, 0, Math.PI * 2);
+  ctx.fill();
+
+  //Using setTimeout instead of window.requestAnimationFrame for slower speed... window.requestAnimationFrame is approximately equal to setTimeout(func, 17);
+  setTimeout(animate, 100);
+}
+animate();
+// === Star animation [ END ] ===
